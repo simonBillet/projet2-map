e6dddc86f5db2f0cd3454ec7a4e8618bb96fe51c
Simon Billet <simon.billet@etudiant.univ-lr.fr>
2022-10-14 15:59:41 +0200

    Ajout de la carte et début de l'ajout du marker

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
+  private map: any;
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.initMap();
+    this.makeCapitalMarkers(this.map);
   }
 
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 47.2055874, -1.5488164 ],
+      zoom: 12
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  makeCapitalMarkers(map: any): void {
+    const lon = 47.2055874;
+    const lat = -1.5488164;
+    const marker = L.marker([lat, lon]);
+    marker.addTo(map);
+
+  }
 }
